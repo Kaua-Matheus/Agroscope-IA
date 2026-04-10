651611c276892fff903b901c67b23e9de57e0349
Kauã Matheus <kauamatheussilva06@gmail.com>
2026-04-09 22:48:56 -0300

    feat: Change Flask to FastAPI

--- Project/IA/app.py
+++ Project/IA/app.py
@@ -1,11 +1,15 @@
-# Flask
-from flask import request, Flask, jsonify
-from flask_cors import CORS
+# FastAPI
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
+import uvicorn
 
 # Scripts auxiliares
 from imports import loadModelWithLabels
 
-# PIL
+# System
+import os
+import io
 from PIL import Image
 
 # Torch
@@ -15,102 +19,140 @@ from torchvision import transforms
 # Numpy
 import numpy as np
 
+from dotenv import load_dotenv
 
-app = Flask(__name__)
-CORS(app)
 
-# Importando os modelos
-GeneralPATH = './models/General/generalAI_v3.pth'
-CornPath = './models/Corn/CornAI_v1.pth'
-SoybeanPath = './models/Soybean/SoybeanAI_v1.pth'
-WheatPath = './models/Wheat/WheatAI_v1.pth'
+# Loading env
+if load_dotenv():
+    print(".env loaded successfully.") # Apply logging
+else:
+    raise("Couldn't load the .env")
+
+
+# Application
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+print(f"Origens: {origins}")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"], # Change for security
+)
+
+# CNN
+## Importing models
+## Generalist
+GeneralPATH = os.getenv("GENERALIST")
+
+## Experts
+CornPath = os.getenv("CORN")
+WheatPath = os.getenv("WHEAT")
+SoybeanPath = os.getenv("SOY")
 
 # Device
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 ## Rede Neural ##
-Net, checkpointNet = loadModelWithLabels("./models/General/generalAI.pth")
-Corn, checkpointCorn = loadModelWithLabels("./models/Corn/cornAI.pth")
-Soybean, checkpointSoybean = loadModelWithLabels("./models/Soybean/soybeanAI.pth")
-Wheat, checkpointWheat = loadModelWithLabels("./models/Wheat/wheatAI.pth")
+# Net, checkpointNet = loadModelWithLabels(GeneralPATH)
+# Corn, checkpointCorn = loadModelWithLabels(CornPath)
+# Wheat, checkpointWheat = loadModelWithLabels(WheatPath)
+# Soybean, checkpointSoybean = loadModelWithLabels(SoybeanPath)
 
-## Transform Geral ## # Pode ser interessante colocar em um módulo separado
+
+# Transform - Apply models.Get_Transform
 transform = transforms.Compose([
-    transforms.Resize((198, 198)),
-    transforms.RandomRotation(4),
-    transforms.RandomHorizontalFlip(),
-    transforms.CenterCrop((192, 192)),
-
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
     transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    transforms.Normalize([0.485,0.456,0.406], [0.229,0.224,0.225]),
 ])
 
 
-## Rodando a aplicação ##
-@app.route('/predict', methods=['POST'])
-def predict():
-    if 'image' not in request.files:
-        return jsonify({"error": "Nenhuma imagem enviada"}), 400
+# Routes
+@app.get("/")
+def Debug():
+    return "The API is working."
 
-    file = request.files['image']
-    test_image = Image.open(file.stream).convert('RGB')
+## Debug
 
-    # Pré-processar a imagem
-    test_image = transform(test_image).unsqueeze(0).to(DEVICE)
 
-    # Previsão
-    with torch.no_grad():
-        output = Net(test_image)
-        generalProbabilities = torch.softmax(output, dim=1).cpu().numpy()
-        predicted_index = np.argmax(generalProbabilities)        
-        generalPrediction = checkpointNet["class_names"][predicted_index]
 
-        generalConfidence = float(generalProbabilities[0][predicted_index])
 
-        match(generalPrediction):
-            case "Corn":
-                test_image = Image.open(file.stream).convert('RGB')
-                test_image = transform(test_image).unsqueeze(0).to(DEVICE)
+# ## Rodando a aplicação ##
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     if 'image' not in request.files:
+#         return jsonify({"error": "Nenhuma imagem enviada"}), 400
 
-                output = Corn(test_image)
-                probabilities = torch.softmax(output, dim=1).cpu().numpy()
-                predicted_index = np.argmax(probabilities)        
-                predicted_class = checkpointCorn["class_names"][predicted_index]
+#     file = request.files['image']
+#     test_image = Image.open(file.stream).convert('RGB')
 
-                sicknessConfidence = float(probabilities[0][predicted_index])
+#     # Pré-processar a imagem
+#     test_image = transform(test_image).unsqueeze(0).to(DEVICE)
+
+#     # Previsão
+#     with torch.no_grad():
+#         output = Net(test_image)
+#         generalProbabilities = torch.softmax(output, dim=1).cpu().numpy()
+#         predicted_index = np.argmax(generalProbabilities)        
+#         generalPrediction = checkpointNet["class_names"][predicted_index]
+
+#         generalConfidence = float(generalProbabilities[0][predicted_index])
+
+#         match(generalPrediction):
+#             case "Corn":
+#                 test_image = Image.open(file.stream).convert('RGB')
+#                 test_image = transform(test_image).unsqueeze(0).to(DEVICE)
+
+#                 output = Corn(test_image)
+#                 probabilities = torch.softmax(output, dim=1).cpu().numpy()
+#                 predicted_index = np.argmax(probabilities)        
+#                 predicted_class = checkpointCorn["class_names"][predicted_index]
+
+#                 sicknessConfidence = float(probabilities[0][predicted_index])
                 
-            case "Soybean":
-                test_image = Image.open(file.stream).convert('RGB')
-                test_image = transform(test_image).unsqueeze(0).to(DEVICE)
+#             case "Soybean":
+#                 test_image = Image.open(file.stream).convert('RGB')
+#                 test_image = transform(test_image).unsqueeze(0).to(DEVICE)
 
-                output = Soybean(test_image)
-                probabilities = torch.softmax(output, dim=1).cpu().numpy()
-                predicted_index = np.argmax(probabilities)        
-                predicted_class = checkpointSoybean["class_names"][predicted_index]
+#                 output = Soybean(test_image)
+#                 probabilities = torch.softmax(output, dim=1).cpu().numpy()
+#                 predicted_index = np.argmax(probabilities)        
+#                 predicted_class = checkpointSoybean["class_names"][predicted_index]
 
-                sicknessConfidence = float(probabilities[0][predicted_index])
+#                 sicknessConfidence = float(probabilities[0][predicted_index])
 
-            case "Wheat":
-                test_image = Image.open(file.stream).convert('RGB')
-                test_image = transform(test_image).unsqueeze(0).to(DEVICE)
+#             case "Wheat":
+#                 test_image = Image.open(file.stream).convert('RGB')
+#                 test_image = transform(test_image).unsqueeze(0).to(DEVICE)
 
-                output = Wheat(test_image)
-                probabilities = torch.softmax(output, dim=1).cpu().numpy()
-                predicted_index = np.argmax(probabilities)        
-                predicted_class = checkpointWheat["class_names"][predicted_index]
+#                 output = Wheat(test_image)
+#                 probabilities = torch.softmax(output, dim=1).cpu().numpy()
+#                 predicted_index = np.argmax(probabilities)        
+#                 predicted_class = checkpointWheat["class_names"][predicted_index]
 
-                sicknessConfidence = float(probabilities[0][predicted_index])
+#                 sicknessConfidence = float(probabilities[0][predicted_index])
 
-            case _:
-                predicted_class = "doença não identificada."
+#             case _:
+#                 predicted_class = "doença não identificada."
 
-    return jsonify({'plant': generalPrediction.lower(), 
-                    'plantConfidence': generalConfidence,
-                    'prediction': predicted_class.lower(), 
-                    'predictionConfidence': sicknessConfidence}), 200
+#     return jsonify({'plant': generalPrediction.lower(), 
+#                     'plantConfidence': generalConfidence,
+#                     'prediction': predicted_class.lower(), 
+#                     'predictionConfidence': sicknessConfidence}), 200
 
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    uvicorn.run(app, host='0.0.0.0', port=5000)
--- Project/utils/models.py
+++ Project/utils/models.py
@@ -12,32 +12,19 @@ class TransformType(Enum):
 def Get_Transform(type: TransformType):
     if type == TransformType.TRAIN:
         return transforms.Compose([
-            transforms.Resize((224, 224)),
-
-            # Implementação para teste
-            # transforms.RandomRotation(15),
-            # transforms.RandomHorizontalFlip(p=0.5),
-            # transforms.RandomVerticalFlip(p=0.2),
-            # transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.1),
-            # transforms.RandomResizedCrop(192, scale=(0.8, 1.0)),
-            # transforms.RandomApply([
-            #     transforms.GaussianBlur(kernel_size=3, sigma=(0.1, 2.0))
-            # ], p=0.3),
-
-            transforms.CenterCrop(192),
+            transforms.Resize(256),
+            transforms.RandomResizedCrop(224, scale=(0.85, 1.0)),
+            transforms.RandomHorizontalFlip(0.5),
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], 
-                            std=[0.229, 0.224, 0.225])
+            transforms.Normalize([0.485,0.456,0.406], [0.229,0.224,0.225]),
         ])
     elif type == TransformType.VAL:
         
         return transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.CenterCrop(192),
-        transforms.ToTensor(),
-        # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], 
-                            std=[0.229, 0.224, 0.225])
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485,0.456,0.406], [0.229,0.224,0.225]),
     ])
 
 
